7e32bf01ba46917d1087684de6c3b036dd25930c
Kevin Luna <kevin.luna200344@gmail.com>
2023-02-24 00:35:13 -0600

    Project 07Simbolo finished.

--- Proyectos/Unidad-1/07Simbolo/src/simbolo/PanelSimbolo.java
+++ Proyectos/Unidad-1/07Simbolo/src/simbolo/PanelSimbolo.java
@@ -14,19 +14,39 @@ import javax.swing.JPanel;
  */
 public class PanelSimbolo extends JPanel {
 
-  public PanelSimbolo() {
-    this.setBackground(Color.CYAN);
-  }
+    public PanelSimbolo() {
+        this.setBackground(Color.CYAN);
+    }
 
-  @Override
-  public void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    int ancho = this.getWidth();
-    int alto = this.getHeight();
-    g.translate(ancho / 2, alto / 2);  // Se mueve el origen al centro del panel
-    // Círculo amarillo
-    int rExterior = 250;
-    g.setColor(Color.YELLOW);
-    g.fillOval(-rExterior, -rExterior, 2 * rExterior, 2 * rExterior);
-  }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        int ancho = this.getWidth();
+        int alto = this.getHeight();
+        g.translate(ancho / 2, alto / 2);  // Se mueve el origen al centro del panel
+        // Círculo amarillo
+        int rExterior = 250;
+        g.setColor(Color.YELLOW);
+        g.fillOval(-rExterior, -rExterior, 2 * rExterior, 2 * rExterior);
+        g.setColor(Color.BLACK);
+        g.drawOval(-rExterior, -rExterior, 2 * rExterior, 2 * rExterior);
+        // Arcos
+        int rArcos = 230;
+        //   g.fillArc(-rArcos, -rArcos, rArcos * 2, rArcos * 2, 0, 60);
+        //   g.fillArc(-rArcos, -rArcos, rArcos * 2, rArcos * 2, 120, 60);
+        //   g.fillArc(-rArcos, -rArcos, rArcos * 2, rArcos * 2, 240, 60);   
+        int nArcos = 3;
+        int factor = 360 / nArcos;
+        for (int i = 0; i < nArcos; i++) {
+            g.fillArc(-rArcos, -rArcos, rArcos * 2, rArcos * 2, factor * i, factor / 2);
+        }
+        // Círculo amarillo interior
+        int rInterior = 100;
+        g.setColor(Color.YELLOW);
+        g.fillOval(-rInterior, -rInterior, rInterior * 2, rInterior * 2);
+        // Círculo negro interior
+        int rCentro = 80;
+        g.setColor(Color.BLACK);
+        g.fillOval(-rCentro, -rCentro, rCentro * 2, rCentro * 2);
+    }
 }
