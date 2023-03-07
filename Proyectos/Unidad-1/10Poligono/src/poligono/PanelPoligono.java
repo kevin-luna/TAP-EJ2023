e6ec25fd53f2d675fbebef1ffe8375c6053548d9
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-07 09:35:46 -0600

    Reading from the file in 10Poligono implemented.

--- Proyectos/Unidad-1/10Poligono/src/poligono/PanelPoligono.java
+++ Proyectos/Unidad-1/10Poligono/src/poligono/PanelPoligono.java
@@ -6,19 +6,42 @@ package poligono;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.io.InputStream;
+import java.util.ArrayList;
 import javax.swing.JPanel;
+import util.Archivo;
 
 /**
  *
  * @author Kevin Alejandro Gutierrez Luna
  */
-public class PanelPoligono extends JPanel{
-  public PanelPoligono(){
+public class PanelPoligono extends JPanel {
+
+  private int[] x;
+  private int[] y;
+
+  public PanelPoligono() {
     this.setBackground(Color.CYAN);
+    abrirArchivo();
   }
-  
+
+  public void abrirArchivo() {
+    String archivo = "/datos/Otro.txt";
+    InputStream flujo = getClass().getResourceAsStream(archivo);
+    ArrayList<String> lineas = Archivo.leerArchivo(flujo);
+    x = new int[lineas.size()];
+    y = new int[lineas.size()];
+    for (int i = 0; i < lineas.size(); i++) {
+      System.out.println(lineas.get(i));
+    }
+  }
+
   @Override
-  public void paintComponent(Graphics g){
+  public void paintComponent(Graphics g) {
     super.paintComponent(g);
+    if (x != null) {
+      g.setColor(Color.BLACK);
+      g.fillPolygon(x, y, x.length);
+    }
   }
 }
