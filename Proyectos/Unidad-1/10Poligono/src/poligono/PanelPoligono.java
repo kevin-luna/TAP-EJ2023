c8fb5a4a778816c8ea7b505e33312ac698eb7077
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-09 16:19:53 -0600

    Now the polygon is drawn in 10Poligono.

--- Proyectos/Unidad-1/10Poligono/src/poligono/PanelPoligono.java
+++ Proyectos/Unidad-1/10Poligono/src/poligono/PanelPoligono.java
@@ -19,6 +19,7 @@ public class PanelPoligono extends JPanel {
 
   private int[] x;
   private int[] y;
+  private int j=0;
 
   public PanelPoligono() {
     this.setBackground(Color.CYAN);
@@ -26,13 +27,19 @@ public class PanelPoligono extends JPanel {
   }
 
   public void abrirArchivo() {
-    String archivo = "/datos/Otro.txt";
+    String archivo = "/datos/Puntos.txt";
     InputStream flujo = getClass().getResourceAsStream(archivo);
     ArrayList<String> lineas = Archivo.leerArchivo(flujo);
     x = new int[lineas.size()];
     y = new int[lineas.size()];
     for (int i = 0; i < lineas.size(); i++) {
       System.out.println(lineas.get(i));
+      String linea = lineas.get(i);
+      String []tokens = linea.split(",");
+      if(tokens.length==2){
+        x[j] = Integer.parseInt(tokens[0].trim());
+        y[j++] = Integer.parseInt(tokens[1].trim());
+      }
     }
   }
 
@@ -40,8 +47,10 @@ public class PanelPoligono extends JPanel {
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
     if (x != null) {
+      g.setColor(Color.BLUE);
+      g.fillPolygon(x, y, j);
       g.setColor(Color.BLACK);
-      g.fillPolygon(x, y, x.length);
+      g.drawPolygon(x, y, j);
     }
   }
 }
