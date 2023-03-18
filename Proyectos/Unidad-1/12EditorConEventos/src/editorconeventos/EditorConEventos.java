2869099fd2d02c5e5128ed17045c6d22f96606ec
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-18 11:58:43 -0600

    Project 12EditorConEventos finished.

---
+++ Proyectos/Unidad-1/12EditorConEventos/src/editorconeventos/EditorConEventos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package editorconeventos;
+
+/**
+ *
+ * @author Kevin Alejandro Gutierrez Luna
+ */
+public class EditorConEventos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaEditor f = new VentanaEditor();
+        f.setSize(800,600);
+        f.setLocation(100,100);
+        f.setVisible(true);
+    }
+
+}
