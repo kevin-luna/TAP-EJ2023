eb7886634698e63173eb61b80b2c7163371491cd
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-20 22:32:13 -0600

    Project 14PanelesConHerencia added.

---
+++ Proyectos/Unidad-1/14PanelesConHerencia/src/penelesconherencia/PanelHerencia.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package penelesconherencia;
+
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JTabbedPane;
+
+/**
+ *
+ * @author Kevin Alejandro Gutierrez Luna
+ */
+public class PanelHerencia extends JTabbedPane{
+    public PanelHerencia(){
+        addComponentes();
+    }
+    
+    private void addComponentes(){
+        setBackground(Color.LIGHT_GRAY);
+        setBorder(BorderFactory.createLineBorder(getBackground(),20));
+        setOpaque(true);
+    }
+}
---
+++ Proyectos/Unidad-1/14PanelesConHerencia/src/penelesconherencia/PanelesConHerencia.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package penelesconherencia;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Kevin Alejandro Gutierrez Luna
+ */
+public class PanelesConHerencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame f = new JFrame("Paneles con herencia");
+        PanelHerencia panel = new PanelHerencia();
+        f.add(panel);
+        f.setSize(800,600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        
+    }
+    
+}
