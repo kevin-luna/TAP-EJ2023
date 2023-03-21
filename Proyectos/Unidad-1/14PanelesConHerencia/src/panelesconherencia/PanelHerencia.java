fb747fb0accdf3eedfdac3d6510ad579beca5a15
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-21 09:13:24 -0600

    Package name of 14PanelesConHerencia changed.

--- Proyectos/Unidad-1/14PanelesConHerencia/src/panelesconherencia/PanelHerencia.java
+++ Proyectos/Unidad-1/14PanelesConHerencia/src/panelesconherencia/PanelHerencia.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package penelesconherencia;
+package panelesconherencia;
 
 import java.awt.Color;
 import javax.swing.BorderFactory;
--- Proyectos/Unidad-1/14PanelesConHerencia/src/panelesconherencia/PanelesConHerencia.java
+++ Proyectos/Unidad-1/14PanelesConHerencia/src/panelesconherencia/PanelesConHerencia.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package penelesconherencia;
+package panelesconherencia;
 
 import javax.swing.JFrame;
 
