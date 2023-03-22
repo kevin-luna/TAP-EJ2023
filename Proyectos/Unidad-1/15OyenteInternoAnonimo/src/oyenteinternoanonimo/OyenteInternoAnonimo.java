118f807bfd5ab4432ec67794b243cbf9f57c3090
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-03-22 09:38:56 -0600

    Project 15OyenteInternoAnonimo finished.

---
+++ Proyectos/Unidad-1/15OyenteInternoAnonimo/src/oyenteinternoanonimo/OyenteInternoAnonimo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package oyenteinternoanonimo;
+
+/**
+ *
+ * @author Kevin Alejandro Gutierrez Luna
+ */
+public class OyenteInternoAnonimo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana f = new Ventana();
+        f.setSize(800,600);
+        f.setLocation(100,100);
+        f.setVisible(true);
+    }
+
+}
