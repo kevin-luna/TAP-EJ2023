eb2c2fb74fed34de0f0b23f72b759c50cf4f330d
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-05-22 22:06:30 -0600

    Some progress in project 03HilosContadores.

---
+++ Proyectos/Unidad-2/03HilosContadoresGrafico/src/hiloscontadoresgrafico/HilosContadoresGrafico.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package hiloscontadoresgrafico;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Kevin Alejandro Gutierrez Luna
+ */
+public class HilosContadoresGrafico {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame f = new JFrame("Contadores");
+        PanelContador panel = new PanelContador();
+        f.setSize(400,600);
+        f.setLocation(100,100);
+        f.add(panel);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+
+}
