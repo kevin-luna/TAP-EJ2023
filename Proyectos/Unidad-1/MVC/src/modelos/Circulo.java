da17d19c7ebb3598f0b29f86896fb35a8c8edd74
kevin-luna <L21020397@veracruz.tecnm.mx>
2023-06-02 10:17:28 -0600

    Model Circulo modified in MVC library.

--- Proyectos/Unidad-1/MVC/src/modelos/Circulo.java
+++ Proyectos/Unidad-1/MVC/src/modelos/Circulo.java
@@ -6,7 +6,9 @@ package modelos;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 
 /**
  *
@@ -34,10 +36,12 @@ public class Circulo extends Figura{
    
    @Override
    public void dibujar(Graphics g){
-     g.setColor(color);
-     g.fillOval(x-radio,y-radio,2*radio,2*radio);
-     g.setColor(Color.BLACK);
-     g.drawOval(x-radio,y-radio,2*radio,2*radio);
+     Graphics2D g2 = (Graphics2D)g;
+     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+     g2.setColor(color);
+     g2.fillOval(x-radio,y-radio,2*radio,2*radio);
+     g2.setColor(Color.BLACK);
+     g2.drawOval(x-radio,y-radio,2*radio,2*radio);
    }
    
    @Override
